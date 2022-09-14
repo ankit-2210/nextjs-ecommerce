a7de539c51f5b0e9e30028b6b11776b659a26ea8
ankit-2210 <78948475+ankit-2210@users.noreply.github.com>
2022-09-14 18:06:42 +0530

    initiala

--- pages/admin/_app.js
+++ pages/admin/_app.js
@@ -8,6 +8,8 @@ import theme from "../../src/theme/theme";
 import createEmotionCache from "../../src/createEmotionCache";
 import FullLayout from "../../src/layouts/FullLayout";
 import "../../styles/style.css";
+import 'bootstrap/dist/css/bootstrap.css';
+
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
